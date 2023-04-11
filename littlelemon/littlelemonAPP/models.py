aa6603bd2c00004842f470956992d8eb145e1d45
Yara Nasser <72654303+yaranasserr@users.noreply.github.com>
2023-04-11 23:24:51 +0200

    models

--- littlelemon/littlelemonAPP/models.py
+++ littlelemon/littlelemonAPP/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
 # Create your models here.
+class Booking(models.Model):
+   
+    Name = models.CharField(max_length=255)
+    BookingDate = models.DateField()
+    No_of_guests = models.SmallIntegerField(6)
+   
+
+
+# Add code to create Menu model
+class Menu(models.Model):
+
+   Title = models.CharField(max_length=255) 
+   price = models.DecimalField(max_digits=10, decimal_places=2)
+   inventory=models.IntegerField(5)
+
